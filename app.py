674212f6a205afa59f44bb270ab3df62f354d120
R Balaji <rajalbandi2balaji@gmail.com>
2025-09-09 17:52:27 +0530

    multi-container setup

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-print("It worked")
-print("Sure.....")
+import psycopg2 as ps
+
+con_ = ps.connect(
+    dbname="testdb",
+    user="testuser",
+    password="testdbpass",
+    # host="127.0.0.1",
+    # port="5432"
+    host="db"
+)
+
+cr = con_.cursor()
+
+cr.execute('SELECT version();')
+print(cr.fetchone())
