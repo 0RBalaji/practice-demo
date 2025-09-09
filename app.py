e99bb1ac71e7d04d143b8dc3aecc876300ca1a84
R Balaji <rajalbandi2balaji@gmail.com>
2025-09-09 20:04:01 +0530

    multi-container setup

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-print("It worked")
-print("Sure.....")
+import os
+import psycopg2 as ps
+
+DB_HOST = os.environ.get("DB_HOST", "localhost")       # default to service name
+DB_PORT = int(os.environ.get("DB_PORT", 5432))
+DB_NAME = os.environ.get("DB_NAME", "testdb")
+DB_USER = os.environ.get("DB_USER", "testuser")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "testpass")
+
+con_ = ps.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
+cr = con_.cursor()
+
+cr.execute('SELECT version();')
+print(cr.fetchone())
